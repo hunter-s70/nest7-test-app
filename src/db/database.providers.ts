9e123d549f176176f785c775e8e70e312d7ba137
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-11-12 20:54:49 +0200

    install typeORM postgres  (#1)
    * expand database env variables

---
+++ src/db/database.providers.ts
@@ -0,0 +1,19 @@
+import { createConnection } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async () => await createConnection({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [
+        __dirname + '/../**/*.entity{.ts,.js}',
+      ],
+      synchronize: true,
+    }),
+  },
+];
