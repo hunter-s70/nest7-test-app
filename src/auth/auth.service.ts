27db6f0d31d149eddd114e563866e894ffcfe88d
serg_buht <serg.buhtoyarov@gmail.com>
2020-11-14 15:21:13 +0200

    reorganize auth service for nest/typeorm

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserService } from '../users/user.service';
 import { User } from '../users/user.entity';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private readonly userService: UserService,
+  ) {}
+
   async signIn(user: User): Promise<User> {
     // ToDo: add token generation
     user.token = 'generated_token';
-    return user.save();
+    return this.usersRepository.save(user);
   }
 
   async signOut(user: User): Promise<User> {
     user.token = null;
-    return user.save();
+    return this.usersRepository.save(user);
   }
 }
