9b336eec1fe38f5800db94ebb89884800e08abf2
serg_buht <serg.buhtoyarov@gmail.com>
2020-11-14 19:45:29 +0200

    add signUp user

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../users/user.entity';
+import { UserModule } from '../users/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
