cf4e5a7ab6b7e8116e35b2293351f59319e81ca9
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-11-23 20:05:59 +0200

    setup typeorm cli and db connection (#6)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,17 @@
 require('dotenv').config();
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+// modules list
 import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { User } from './users/user.entity';
-import { Article } from './articles/article.entity';
 import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(
-      {
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        entities: [User, Article],
-        synchronize: true,
-      }
-    ),
+    TypeOrmModule.forRoot(),
     UserModule,
     AuthModule,
     ArticlesModule,
