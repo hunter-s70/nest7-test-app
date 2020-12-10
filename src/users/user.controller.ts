a2a15fbe4b49e551a78b1e0fca2ff0eea9fd4f07
serg_buht <serg.buhtoyarov@gmail.com>
2020-12-10 21:11:27 +0200

    add serialization

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Get,
+  Controller,
+  UseInterceptors,
+  ClassSerializerInterceptor
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -9,6 +14,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @UseInterceptors(ClassSerializerInterceptor)
   @ApiResponse({ status: 200, description: 'Users list.', type: [User]})
   async getUsers(): Promise<User[]> {
     try {
