1f71d9cef3d57e7c520208c5a7633189da775eb8
serg_buht <serg.buhtoyarov@gmail.com>
2020-12-10 19:53:58 +0200

    change api path

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
